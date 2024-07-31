b45176d42d73b74f65d48506b6c167349ffc4ee7
pr0fy11 <dimitarvelev01@gmail.com>
2024-07-31 20:03:36 +0300

    small update

--- scraper.py
+++ scraper.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup 
 import requests
-
+import csv
 
 cars = requests.get("https://www.mobile.bg/")
 soup = BeautifulSoup(cars.text,"html.parser")
 names = soup.findAll("div", attrs={"class":"ime"})
 prices = soup.findAll("div", attrs={"class":"cena"})
+file = open("scrapedData.csv", 'w')
+writer = csv.writer(file)
 
-for name in names:
-    print(name.text)
-for price in prices:
-    print(price.text)
+writer.writerow(["TITLE", "PRICE"])
+
+for name, price in zip(names, prices):
+    print(name.text + ' ' + price.text)
+    writer.writerow([name.text, price.text])
+file.close
